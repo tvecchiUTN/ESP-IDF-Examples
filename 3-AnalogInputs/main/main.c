c764abc7ecb88a41c39e92f89af37497fe261dd0
Tomas Vecchi <204607402+tvecchiUTN@users.noreply.github.com>
2025-12-16 12:34:28 -0300

    Comentado de codigo ejemplo 3

--- 3-AnalogInputs/main/main.c
+++ 3-AnalogInputs/main/main.c
@@ -2,13 +2,16 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
-#include "esp_adc/adc_oneshot.h"
 #include "esp_log.h"
+//Libreria utilizada
+#include "esp_adc/adc_oneshot.h"
 
 static const char *TAG = "Ejemplo 3 entrada analoga";
 
+//Funcion que crea la configuracion del potenciometro
 void init_analog_in(adc_oneshot_unit_handle_t *pot_handle)
 {
+    //Configuracion inicial del potenciometro
     adc_oneshot_unit_init_cfg_t pot_conf = {};
     pot_conf.unit_id = ADC_UNIT_1;
     pot_conf.ulp_mode = ADC_ULP_MODE_DISABLE;
@@ -16,6 +19,7 @@ void init_analog_in(adc_oneshot_unit_handle_t *pot_handle)
 
     ESP_ERROR_CHECK(adc_oneshot_new_unit(&pot_conf, pot_handle));
 
+    //Configuracion del canal
     adc_oneshot_chan_cfg_t pot_chan = {};
     pot_chan.atten = ADC_ATTEN_DB_12;
     pot_chan.bitwidth = ADC_BITWIDTH_DEFAULT;
@@ -28,21 +32,26 @@ void init_analog_in(adc_oneshot_unit_handle_t *pot_handle)
 
 void app_main(void)
 {
+    //Handle o manejador del potenciometro
     adc_oneshot_unit_handle_t pot_handle;
 
+    //Establece la configuracion
     init_analog_in(&pot_handle);
 
     while(1)
     {
+        //Variable que guarda los valores del potenciometro
         int entrada = 0;
 
+        //Lee los valores recibidos
         ESP_ERROR_CHECK(adc_oneshot_read(pot_handle, ADC_CHANNEL_6, &entrada));
 
         printf("%d\n", entrada);
 
-        //ESP_LOGI(TAG, "Señal detectada");}
+        //Delay de 100 milisegundos
         vTaskDelay(pdMS_TO_TICKS(100));
     }
 
+    //Elimina el handle y libera recursos
     adc_oneshot_del_unit(pot_handle);
 }
