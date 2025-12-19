1519462812847af7305e8abf214343c20fed3549
Tomas Vecchi <204607402+tvecchiUTN@users.noreply.github.com>
2025-12-19 01:18:51 -0300

    ups

---
+++ Iniciales/6.1-dacVoltajes/main/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "esp_adc/adc_cali.h"
+#include "esp_adc/adc_cali_scheme.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void app_main(void)
+{
+
+}
