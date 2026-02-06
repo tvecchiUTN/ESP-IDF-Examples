495526a27d1c457e08319dce54683f325acb949f
Tomas Vecchi <204607402+tvecchiUTN@users.noreply.github.com>
2026-02-06 19:47:47 -0300

    Calibracion y lectura

--- Examples/Iniciales/06-sensor_touch/main/main.c
+++ Examples/Iniciales/06-sensor_touch/main/main.c
@@ -36,13 +36,35 @@ static esp_err_t init_touch_pad(int chan_id, touch_sensor_handle_t handle_touch_
         return ESP_ERR_INVALID_ARG;
     }
 
+    //Flag para analizar si se habilito el sensor touch
+    static uint8_t flag_is_enable_touch = 1;
+
     touch_channel_config_t cfg_channel_touch = {0};
     cfg_channel_touch.abs_active_thresh[0] = 100;
     
-
     esp_err_t err = touch_sensor_new_channel(handle_touch_sensor, chan_id, &cfg_channel_touch, handle_channel_sensor);
     
     ESP_RETURN_ON_ERROR(err, ERR_TAG, "Fallo al crear canal %d", chan_id);
+
+    if(flag_is_enable_touch)
+    {
+        flag_is_enable_touch = 0;
+
+        touch_sensor_enable(handle_touch_sensor);
+    }
+
+    return ESP_OK;
+}
+
+static esp_err_t perform_cali(touch_sensor_handle_t handle_touch_sensor)
+{
+    touch_sensor_filter_config_t cfg_filter = {
+        .data_filter_fn = NULL,
+        .interval_ms = 500,
+        .user_filter_ctx = NULL,
+    };
+
+    touch_sensor_config_filter(handle_touch_sensor, &cfg_filter);
 
     return ESP_OK;
 }
@@ -54,11 +76,25 @@ void app_main(void)
 
     ESP_ERROR_CHECK(init_controller(&handle_sensor_touch));
 
-    ESP_ERROR_CHECK(init_touch_pad(0, handle_sensor_touch, &handle_channel_0));
+    ESP_ERROR_CHECK(init_touch_pad(3, handle_sensor_touch, &handle_channel_0));
 
+    ESP_ERROR_CHECK(perform_cali(handle_sensor_touch));
+
+    uint32_t initValue = 0;
+
+    touch_channel_read_data(handle_channel_0, TOUCH_CHAN_DATA_TYPE_RAW, &initValue);
+
+    uint32_t readValue = 0;
     while(1)
     {
-        ESP_LOGI("TESTING", "Probando la placa");
+        touch_channel_read_data(handle_channel_0, TOUCH_CHAN_DATA_TYPE_RAW, &readValue);
+        if(readValue < initValue)
+        {
+            ESP_LOGI("Salida estandar", "Parece que se presiono");
+        }
+
+        ESP_LOGI("Salida estandar", "El valor actual es %d", readValue);
+
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
