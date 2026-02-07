072e49ba90abc8540d5283992947ff8cb7cbba44
Tomas Vecchi <204607402+tvecchiUTN@users.noreply.github.com>
2026-02-07 02:34:52 -0300

    Lectura de datos

--- Examples/Iniciales/06-sensor_touch/main/main.c
+++ Examples/Iniciales/06-sensor_touch/main/main.c
@@ -6,7 +6,9 @@
 #include "esp_check.h"
 #include "driver/touch_sens.h"
 
-static const char* ERR_TAG = "Salida de errores - modulo touch pad";
+//static const char* ERR_TAG = "Salida de errores - modulo touch pad";
+
+static const char* TAG = "TOUCH_DRIVER";
 
 #define SAMPLE_NUM 1
 
@@ -24,7 +26,7 @@ static esp_err_t init_controller(touch_sensor_handle_t *handle_touch_sensor)
     esp_err_t err;
     err = touch_sensor_new_controller(&cfg_touch, handle_touch_sensor);
 
-    ESP_RETURN_ON_ERROR(err, ERR_TAG, "Fallo al crear controlador global");
+    ESP_RETURN_ON_ERROR(err, TAG, "Fallo al crear controlador global");
 
     return ESP_OK;
 }
@@ -36,65 +38,94 @@ static esp_err_t init_touch_pad(int chan_id, touch_sensor_handle_t handle_touch_
         return ESP_ERR_INVALID_ARG;
     }
 
-    //Flag para analizar si se habilito el sensor touch
-    static uint8_t flag_is_enable_touch = 1;
-
     touch_channel_config_t cfg_channel_touch = {0};
     cfg_channel_touch.abs_active_thresh[0] = 100;
     
     esp_err_t err = touch_sensor_new_channel(handle_touch_sensor, chan_id, &cfg_channel_touch, handle_channel_sensor);
     
-    ESP_RETURN_ON_ERROR(err, ERR_TAG, "Fallo al crear canal %d", chan_id);
+    ESP_RETURN_ON_ERROR(err, TAG, "Fallo al crear canal %d", chan_id);
 
-    if(flag_is_enable_touch)
+    return ESP_OK;
+}
+
+static esp_err_t init_filter_touch_pad(touch_sensor_handle_t handle_touch_sensor)
+{
+    if(!handle_touch_sensor)
     {
-        flag_is_enable_touch = 0;
-
-        touch_sensor_enable(handle_touch_sensor);
+        return ESP_ERR_INVALID_ARG;
     }
 
-    return ESP_OK;
-}
-
-static esp_err_t perform_cali(touch_sensor_handle_t handle_touch_sensor)
-{
-    touch_sensor_filter_config_t cfg_filter = {
+    touch_sensor_filter_config_t cfg_filter_touch = {
+        .interval_ms = 10,
         .data_filter_fn = NULL,
-        .interval_ms = 500,
-        .user_filter_ctx = NULL,
     };
 
-    touch_sensor_config_filter(handle_touch_sensor, &cfg_filter);
+    return touch_sensor_config_filter(handle_touch_sensor, &cfg_filter_touch);
+}
+
+static esp_err_t calibrate_threshold(touch_sensor_handle_t handle_sensor_touch, touch_channel_handle_t handle_channel_sensor, uint32_t *out_threshold)
+{
+    if(!out_threshold)
+    {
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    uint32_t benchmark_val[1];
+
+    ESP_RETURN_ON_ERROR(touch_channel_read_data(handle_channel_sensor, TOUCH_CHAN_DATA_TYPE_RAW, benchmark_val), TAG, "Fallo al leer");
+
+    *out_threshold = (uint32_t)(benchmark_val[0] * 0.8);
+
+    touch_sensor_stop_continuous_scanning(handle_sensor_touch);
+    touch_sensor_disable(handle_sensor_touch);
+
+    touch_channel_config_t recfg_chan = {0};
+    recfg_chan.abs_active_thresh[0] = *out_threshold;
+
+    touch_sensor_reconfig_channel(handle_channel_sensor, &recfg_chan);
+
+    touch_sensor_start_continuous_scanning(handle_sensor_touch);
+    touch_sensor_enable(handle_sensor_touch);
 
     return ESP_OK;
 }
+
+#define CHAN_USER 3
 
 void app_main(void)
 {
     touch_sensor_handle_t handle_sensor_touch = NULL;
     touch_channel_handle_t handle_channel_0 = NULL;
+    uint32_t threshold = 0;
 
     ESP_ERROR_CHECK(init_controller(&handle_sensor_touch));
 
-    ESP_ERROR_CHECK(init_touch_pad(3, handle_sensor_touch, &handle_channel_0));
+    ESP_ERROR_CHECK(init_filter_touch_pad(handle_sensor_touch));
 
-    ESP_ERROR_CHECK(perform_cali(handle_sensor_touch));
+    ESP_ERROR_CHECK(init_touch_pad(CHAN_USER, handle_sensor_touch, &handle_channel_0));
 
-    uint32_t initValue = 0;
+    ESP_ERROR_CHECK(touch_sensor_enable(handle_sensor_touch));
 
-    touch_channel_read_data(handle_channel_0, TOUCH_CHAN_DATA_TYPE_RAW, &initValue);
+    ESP_ERROR_CHECK(touch_sensor_start_continuous_scanning(handle_sensor_touch));
 
-    uint32_t readValue = 0;
+    vTaskDelay(pdMS_TO_TICKS(500));
+
+    ESP_ERROR_CHECK(calibrate_threshold(handle_sensor_touch, handle_channel_0, &threshold));
+
+    uint32_t current_val[1];
+
     while(1)
     {
-        touch_channel_read_data(handle_channel_0, TOUCH_CHAN_DATA_TYPE_RAW, &readValue);
-        if(readValue < initValue)
+        if(touch_channel_read_data(handle_channel_0, TOUCH_CHAN_DATA_TYPE_RAW, current_val) == ESP_OK)
         {
-            ESP_LOGI("Salida estandar", "Parece que se presiono");
+            if(current_val[0] < threshold)
+            {
+                ESP_LOGI(TAG, "TOCADO! Val: %lu < Thr: %lu", current_val[0], threshold);
+            }
         }
 
-        ESP_LOGI("Salida estandar", "El valor actual es %d", readValue);
+        ESP_LOGI(TAG, "El valor actual es %d", current_val[0]);
 
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
