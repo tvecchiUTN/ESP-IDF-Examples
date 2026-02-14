3ae0cb7e3185e25b362b4291d9aa8fd8f67efaba
Tomas Vecchi <204607402+tvecchiUTN@users.noreply.github.com>
2026-02-14 00:56:59 -0300

    Preparando para light sleep

---
+++ Examples/Iniciales/10-sleepmodes/main/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "esp_sleep.h"
+#include "driver/rtc_io.h"
+
+void app_main(void)
+{
+    //esp_sleep_enable_timer_wakeup
+
+}
